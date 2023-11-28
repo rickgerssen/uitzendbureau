65aae32501edc32465eb68b79baeeaa67ac90ab2
Gio20016 <248170@cursist.rockopnh.nl>
2023-11-28 15:10:36 +0100

    Added Edit Account Files

---
+++ EditAccountJS.js
@@ -0,0 +1,11 @@
+
+
+$('input[type=file]').each(function() {
+    $(this).on('change', () => {
+        if($(this).val()) {
+            const element = $(this).parent().find('span.identifier')
+            const x = $(this).val().split("\\");
+            element.text(x[x.length - 1]).show()
+        }
+    });
+});
