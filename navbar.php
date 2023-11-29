e1414776bb6b41b6822aa1a9599d8a25b6c94e2c
Joost-berkhout <114413928+Joost-berkhout@users.noreply.github.com>
2023-11-29 23:15:00 +0100

    inlog versie 2

--- navbar.php
+++ navbar.php
@@ -8,19 +8,27 @@
             }, 'google_translate_element');
         }
     </script> 
-<nav class="navbar navbar-default">
+        <style>
+        body {
+            padding-top: 70px;
+        }
+    </style>
+    
+<nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
       
       <div class="navbar-header">
-        <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-            <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+        <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-fixed">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
         </button>
         <a class="navbar-brand" href="#">
             <img src="images/logo.png" width="30" height="30" class="">
         </a>
     </div>
     
-        <div class="collapse navbar-collapse" id="navbar-collapse">
+        <div class="collapse navbar-collapse" id="navbar-collapse-fixed">
           <ul class="nav navbar-nav navbar-right">
               <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown">
