852b5b3a15f3aef6d6677c7091c70507dfca0e29
Thomas Verhage <thomasverhage@outlook.com>
2023-11-29 09:14:39 +0100

    registratiepagina style aanpassing

--- register.php
+++ register.php
@@ -100,10 +100,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="loginstyle.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"><!--link naar stylesheet-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 360px; padding: 20px; }
+        body {
+            background-color: #0047AB;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;
+        }
+        .form-container {
+            width: 90%;
+            max-width: 400px;
+            background: #fff;
+ 
+            padding: 20px;
+            margin-bottom: 20px;
+        }
     </style>
 </head>
 <body>
